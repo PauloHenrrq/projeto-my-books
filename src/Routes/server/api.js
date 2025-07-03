c493d69016c6261692893e734ca952c63b718f52
Emanuel Felicio <emanuelfelicio10@gmail.com>
2025-07-03 10:24:01 -0300

    update

--- src/Routes/server/api.js
+++ src/Routes/server/api.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-export async function APIBooks(query = 'comedy') {
-    const res = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}&maxResults=20`)
-    const data = await res.json()
+export async function APIBooks(queryFilter, query) {
+  try {
+    if (query.length < 2) {
+      throw new Error(`a pesquisa deve ter no mínimo 2 caracteres`);
+    }
+    const res = await fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${queryFilter}${encodeURIComponent(
+        query
+      )}&maxResults=20`
+    );
+
+    if (!res.ok) {
+      throw new Error(`Erro HTTP! Status: ${res.status} - ${res.statusText}`);
+    }
+
+    const data = await res.json();
+    //teste
+    console.log("pesquisa deu certo");
     return data.items || [];
+  } catch (error) {
+    console.error("Ocorreu um erro ao buscar os livros:", error.message);
+  }
 }
 
 // https://www.googleapis.com/books/v1/volumes?q=comedy&maxResults=20&startIndex=18 - Exemplo de pesquisa dinâmica, 1-19, 20-39...
